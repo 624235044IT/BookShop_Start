a2cd84f78c6031e9cb4e1a7c7178029e83ef0db0
624235044 <624235044@parichat.skru.ac.th>
2021-10-08 16:46:16 +0700

    first commit

---
+++ src/components/Cart.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <h2>Books in Cart</h2>
+    <div v-bind:key="book.bookid" v-for="book in cart">
+        <CartItem v-bind:book="book" v-on:decrease:cartitem="Decrease" />
+    </div>
+</div>
+</template>
+
+<script>
+import CartItem from './CartItem.vue';
+export default {
+    name: "Cart",
+    props: ["cart"],
+    components: {
+        CartItem
+    },
+    methods:{
+        Decrease(bookId){
+            this.$emit("decrease:cart",bookId);
+        }
+
+    }
+
+}
+</script>
+
+<style scoped>
+.book-item {
+    background: #ffffff;
+    padding: 10px;
+    border-bottom: 1px rgb(201, 199, 199) dotted;
+}
+</style>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="book-item" v-bind:class="{'is-complete':book.completed}">
+
+    <b>{{book.title}}</b><br />
+    <b>Price:</b> {{book.price}}<br />
+    <b>Qty:</b> {{book.qty}}<br />
+    <div align="center">
+        <button class="btn btn-success">&nbsp;+&nbsp;</button>&nbsp;
+        <button class="btn btn-info" v-on:click="Decrease(book.bookid)">&nbsp;-&nbsp;</button>&nbsp;
+        <button class="btn btn-danger">&nbsp;x&nbsp;</button>
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: "CartItem",
+    props: ["book"],
+    methods:{
+        Decrease(bookId){
+            this.$emit("decrease:cartitem",bookId);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.book-item {
+    background: #e6f7ff;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+</style>
